f71aee656f089162b7ebbffb01d978c4397b81ee
Lucas Grzegorczyk <furai@thd.vg>
2022-04-30 15:16:37 +0200

    Modify how Destination field looks like

--- TrippyWeb/Model/Trip.cs
+++ TrippyWeb/Model/Trip.cs
@@ -5,7 +5,8 @@ namespace TrippyWeb.Model;
 public class Trip
 {
     [Key]
-    public int ID { get; set; }
-    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Destination field is required.")]
+    [StringLength(maximumLength: 100, MinimumLength = 2)]
     public string Destination { get; set; } = String.Empty;
 }
