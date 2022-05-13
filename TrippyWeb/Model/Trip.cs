1155d338c6888899bc7e77477638dc517ff84bd2
Lucas Grzegorczyk <furai@thd.vg>
2022-05-13 18:45:03 +0200

    Update Trip model

--- TrippyWeb/Model/Trip.cs
+++ TrippyWeb/Model/Trip.cs
@@ -6,7 +6,17 @@ public class Trip
 {
     [Key]
     public int Id { get; set; }
+
     [Required(ErrorMessage = "Destination field is required.")]
     [StringLength(maximumLength: 100, MinimumLength = 2)]
     public string Destination { get; set; } = String.Empty;
+
+    [Required(ErrorMessage = "Free Spots field is required.")]
+    [Range(1, 10, ErrorMessage = "Value for {0} must be between {1} and {2}")]
+    [Display(Name = "Free spots")]
+    public int FreeSpots { get; set; }
 }
+
+/* Jako Kierowca określam warunki przejazdu: ilość osób,
+pasażerowie palący/niepalący, cena, miejsce docelowe, ewentualne
+przystanki */
