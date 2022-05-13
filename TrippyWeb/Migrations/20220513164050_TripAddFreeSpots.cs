326c3110d4259d237880437e4d8c37173c9a644e
Lucas Grzegorczyk <furai@thd.vg>
2022-05-13 18:45:35 +0200

    Add database migration for Trips

---
+++ TrippyWeb/Migrations/20220513164050_TripAddFreeSpots.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrippyWeb.Migrations
+{
+    public partial class TripAddFreeSpots : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FreeSpots",
+                table: "Trip",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FreeSpots",
+                table: "Trip");
+        }
+    }
+}
