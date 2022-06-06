1d2bd60b2f65e0cfb1a61f7a688129359030ee4a
Lucas Grzegorczyk <furai@thd.vg>
2022-06-06 18:06:42 +0200

    Clean up scaffolded indentity files

--- TrippyWeb/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ TrippyWeb/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -25,10 +25,6 @@ namespace TrippyWeb.Areas.Identity.Pages.Account
             _signInManager = signInManager;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
 
