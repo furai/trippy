8c5ff33909ea6eb124b571ecea042635ec64698d
Lucas Grzegorczyk <furai@thd.vg>
2022-06-08 14:35:05 +0200

    Add stop entity

---
+++ TrippyWeb/Models/Stop.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrippyWeb.Model;
+
+[Table("Stop")]
+public class Stop
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = String.Empty;
+}
