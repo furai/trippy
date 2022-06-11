c6fa987e9da62693560ce843910d0de1b3787bad
Lucas Grzegorczyk <furai@thd.vg>
2022-06-12 01:50:28 +0200

    Greatly improve filters

--- TrippyWeb/Pages/Index.cshtml.cs
+++ TrippyWeb/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
 #nullable disable
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TrippyWeb.Data;
@@ -10,6 +12,19 @@ namespace TrippyWeb.Pages.Trips
     {
         private readonly TrippyWebDbContext _context;
 
+        [BindProperty(SupportsGet = true)]
+        [Display(Name = "Filters:")]
+        public string Filter { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [Display(Name = "NonSmoking:")]
+        public string NonSmoking { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [Display(Name = "Min Price:")]
+        [Range(0.0, int.MaxValue, ErrorMessage = "Value for {0} must be a positive integer.")]
+        public int Price { get; set; } = 0;
+
         public IndexModel(TrippyWebDbContext context)
         {
             _context = context;
@@ -17,15 +32,22 @@ namespace TrippyWeb.Pages.Trips
 
         public List<Trip> TripsList { get; set; }
 
-        public async Task OnGetAsync(string filter)
+        public async Task OnGetAsync()
         {
-            TripsList = await _context.Trips.Include(t => t.Owner).Include(t => t.Passengers).ToListAsync();
-            TripsList = filter switch
+            TripsList = await _context.Trips.Include(t => t.Owner).Include(t => t.Passengers).Where(t => t.Price >= Price).ToListAsync();
+            TripsList = Filter switch
             {
                 "Active" => TripsList.Where(t => (t.FreeSpots - t.Passengers.Count) > 0 && t.StartDate > DateTime.Now).ToList(),
                 "Full" => TripsList.Where(t => (t.FreeSpots - t.Passengers.Count) < 1).ToList(),
                 "New" => TripsList.Where(t => t.StartDate > DateTime.Now).ToList(),
                 "Expired" => TripsList.Where(t => t.StartDate < DateTime.Now).ToList(),
+                _ => TripsList
+            };
+
+            TripsList = NonSmoking switch
+            {
+                "Yes" => TripsList.Where(t => t.NonSmoking == true).ToList(),
+                "No" => TripsList.Where(t => t.NonSmoking == false).ToList(),
                 _ => TripsList
             };
         }
